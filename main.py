58626aa90f626e7ba300dc8af54671b46f820375
DilshodbekMX <dilshodbekpnu@gmail.com>
2023-08-24 21:07:25 +0500

    Modified

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 import datetime
 import os
 import threading
-import time
 import pytz
 import urllib.request
 import cv2
 import settings
-from PIL import Image
 from yolov8 import YOLOv8
 
 db = settings.FIRESTORE_DB
 storage = settings.FIREBASE_BUCKET
-file_num = 0  # Save images to current directory
-SECONDS_TO_COUNTDOWN = 2
-SECONDS_TO_COUNTDOWN2 = 30
+file_num = 0
+SECONDS_TO_COUNTDOWN = 5
 countdown = SECONDS_TO_COUNTDOWN
-countdown2 = SECONDS_TO_COUNTDOWN2
 countdown_timestamp = cv2.getTickCount()
-camera_urls = []
-camera_names = []
-user_ids = []
-camera_ids = []
+camera_urls, camera_names, user_ids, online_user_ids, camera_ids = [], [], [], [], []
+
 callback_done = threading.Event()
 
 
@@ -60,6 +54,7 @@ def on_snapshot(col_snapshot, changes, read_time):
         camera_urls.append(dict_doc["web_address"])
         user_ids.append(dict_doc["offline_user_id"])
         camera_names.append(dict_doc["camera_name"])
+        online_user_ids.append(dict_doc["online_user_id"])
     callback_done.set()
 
 
@@ -69,130 +64,85 @@ query_watch = col_query.on_snapshot(on_snapshot)
 
 def upload_to_local():
     file_suffix = ".jpg"
-    save_path = settings.MEDIA_ROOT + "/" + "captured_images/"
+    model_path = settings.MEDIA_ROOT + "/yolo_model/"
+    save_path = settings.MEDIA_ROOT + "/captured_images/"
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     global countdown_timestamp, countdown
     i = 0
     while True:
-        res = [file_path for file_path in os.listdir(save_path) if
-               os.path.isfile(os.path.join(save_path, file_path))]
         if len(camera_urls) <= 0:
             break
+        if i % len(camera_urls) == 0 and i >= 1000:
+            i = 0
         name = camera_urls[i % len(camera_urls)]
         if urllib.request.urlopen(name).getcode() == 200:
             vid = cv2.VideoCapture(name)
-            while vid:
+            while True:
                 timestamp = cv2.getTickCount()
                 ret, frame = vid.read()
                 if (timestamp - countdown_timestamp) / cv2.getTickFrequency() > 1.0:
                     file_name = str(user_ids[i % len(camera_urls)]) + "#" + camera_names[i % len(camera_urls)] + "#"
                     countdown_timestamp = cv2.getTickCount()
                     countdown -= 1
+                    print(countdown)
                     if countdown <= 0:
+                        countdown = SECONDS_TO_COUNTDOWN
                         try:
-                            filepath = get_filepath(save_path, file_name, file_suffix)
-                            cv2.imwrite(filepath, frame)
-                            countdown = SECONDS_TO_COUNTDOWN
+                            model_file = model_path + "best.onnx"
+                            yolov8_detector = YOLOv8(model_file, conf_thres=0.2, iou_thres=0.3)
+                            boxes, scores, class_ids = yolov8_detector(frame)
+                            if len(boxes) > 0 and len(scores) > 0 and len(class_ids) > 0:
+                                print("Detected image")
+                                detected_classes = []
+                                offline_user_id = user_ids[i % len(camera_urls)]
+                                online_user_id = online_user_ids[i % len(camera_urls)]
+                                camera_name = camera_names[i % len(camera_urls)]
+                                current_time = datetime.datetime.now(pytz.timezone('Asia/Tashkent'))
+                                for box, score, class_id in zip(boxes, scores, class_ids):
+                                    if class_id == 0:
+                                        detected_classes.append(f"Smoke {round(score * 100, 2)} %")
+                                    else:
+                                        detected_classes.append(f"Fire {round(score * 100, 2)} %")
+                                combined_img = yolov8_detector.draw_detections(frame)
+                                cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
+                                filepath = get_filepath(save_path, file_name, file_suffix)
+                                is_saved = cv2.imwrite(filepath, combined_img, [cv2.IMWRITE_JPEG_QUALITY, 50])
+                                if is_saved:
+                                    print(f"Image saved to {filepath}")
+                                    blob = storage.blob(f"{camera_name}#{current_time}", chunk_size=262144)
+                                    blob.upload_from_filename(filepath)
+                                    blob.make_public()
+                                    new_doc = db.collection("Detected").document()
+                                    print(detected_classes)
+                                    print(offline_user_id)
+                                    print(online_user_id)
+                                    print(camera_name)
+                                    print(current_time)
+                                    data = {
+                                        "detected_classes": detected_classes,
+                                        "camera_name": camera_name,
+                                        "offline_user_id": offline_user_id,
+                                        "online_user_id": online_user_id,
+                                        "id": new_doc.id,
+                                        "detected_time": current_time,
+                                        "image_url": blob.public_url,
+                                    }
+                                    print(data)
+                                    new_doc.set(data)
+                                    os.remove(filepath)
+                            else:
+                                print("No objects detected")
                         except:
-                            pass
-                        break
+                            break
                 if cv2.waitKey(10) == ord('q'):
                     break
             i += 1
             vid.release()
             cv2.destroyAllWindows()
-            print(f"Captured Resources length: {len(res)}")
-            if len(res) >= 60:
-                print("Stopped")
-                break
 
 
-def get_and_detect():
-    file_suffix = ".jpg"
-    save_path = settings.MEDIA_ROOT + "/detected_images/"
-    model_path = settings.MEDIA_ROOT + "/yolo_model/"
-    source_path = settings.MEDIA_ROOT + "/captured_images/"
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-    if not os.path.exists(source_path):
-        os.makedirs(source_path)
-    if not os.path.exists(model_path):
-        os.makedirs(model_path)
-    res = [file_path for file_path in os.listdir(source_path) if
-           os.path.isfile(os.path.join(source_path, file_path))]
-    if len(res) > 0:
-        for image in res:
-            if os.path.isfile(os.path.join(source_path, image)):
-                model_file = model_path + "best.onnx"
-                yolov8_detector = YOLOv8(model_file, conf_thres=0.2, iou_thres=0.3)
-                img_url = os.path.join(source_path, image)
-                img = cv2.imread(img_url)
-                boxes, scores, class_ids = yolov8_detector(img)
-                if len(boxes) > 0 and len(scores) > 0 and len(class_ids) > 0:
-                    print("Detected image")
-                    detected_classes = []
-                    for box, score, class_id in zip(boxes, scores, class_ids):
-                        if class_id == 0:
-                            detected_classes.append(f"Smoke {round(score * 100, 2)} %")
-                        else:
-                            detected_classes.append(f"Fire {round(score * 100, 2)} %")
-                    img_list = image.split("#")
-                    offline_user_id = img_list[0]
-                    camera_name = img_list[1]
-                    new_doc = db.collection("Detected").document()
-                    data = {
-                        "detected_classes": detected_classes,
-                        "camera_name": camera_name,
-                        "offline_user_id": offline_user_id,
-                        "id": new_doc.id,
-                        "detected_time": datetime.datetime.now(pytz.timezone('Asia/Tashkent')),
-                    }
-                    new_doc.set(data)
-                    new_image_name = f"{new_doc.id}#{camera_name}"
-                    combined_img = yolov8_detector.draw_detections(img)
-                    cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
-                    filepath = get_filepath2(save_path, new_image_name, file_suffix)
-                    cv2.imwrite(filepath, combined_img)
-                os.remove(os.path.join(source_path, image))
-            else:
-                break
-
-
-def upload_to_cloud():
-    source_path = settings.MEDIA_ROOT + "/detected_images/"
-    res = [file_path for file_path in os.listdir(source_path) if
-           os.path.isfile(os.path.join(source_path, file_path))]
-    if len(res) > 0:
-        for image in res:
-            if os.path.isfile(os.path.join(source_path, image)):
-                print("Detected image saved to cloud")
-                img_list = image.split("#")
-                doc_id = img_list[0]
-                camera_name = img_list[1]
-                detected_ref = db.collection("Detected").document(doc_id)
-                current_time = datetime.datetime.now(pytz.timezone('Asia/Tashkent'))
-                blob = storage.blob(f"Detected/{camera_name}#{current_time}")
-                blob.upload_from_filename(os.path.join(source_path, image))
-                blob.make_public()
-                data = {
-                    "image_url": blob.public_url,
-                    "detected_time": current_time,
-                }
-                detected_ref.update(data)
-                os.remove(os.path.join(source_path, image))
-            else:
-                break
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     while True:
         print("upload_to_local: ")
         upload_to_local()
-        print("get_and_detect: ")
-        get_and_detect()
-        print("upload_to_cloud: ")
-        upload_to_cloud()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
